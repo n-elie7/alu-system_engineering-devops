7a3b3aba8f45a4ec41907130b6fc9b07c7c12562
Elie <n.elie@alueducation.com>
2026-02-23 18:45:30 +0200

    used puppet to ssh config

---
+++ ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,30 @@
+file { '/home/vagrant/.ssh/config':
+  ensure  => file,
+  mode    => '0600',
+  owner   => 'vagrant',
+  group   => 'vagrant',
+}
+
+file_line { 'Declare identity file':
+  path  => '/home/vagrant/.ssh/config',
+  line  => '    IdentityFile ~/.ssh/school',
+  match => '^\s*IdentityFile',
+}
+
+file_line { 'Turn off passwd auth':
+  path  => '/home/vagrant/.ssh/config',
+  line  => '    PasswordAuthentication no',
+  match => '^\s*PasswordAuthentication',
+}
+
+file_line { 'Force public key auth':
+  path  => '/home/vagrant/.ssh/config',
+  line  => '    PubkeyAuthentication yes',
+  match => '^\s*PubkeyAuthentication',
+}
+
+file_line { 'Define host wildcard':
+  path  => '/home/vagrant/.ssh/config',
+  line  => 'Host *',
+  match => '^Host \*',
+}
