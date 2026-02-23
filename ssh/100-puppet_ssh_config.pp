954842f61282a0be3c72d0f365778afa16b99520
Elie <n.elie@alueducation.com>
2026-02-23 23:28:27 +0200

    improved line format

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -3,12 +3,12 @@ include stdlib
 
 file_line { 'Turn off passwd auth':
   path  => '/etc/ssh/ssh_config',
-  line  => '    PasswordAuthentication no',
-  match => '^.*PasswordAuthentication.*$',
+  line  => 'PasswordAuthentication no',
+  match => '^PasswordAuthentication',
 }
 
 file_line { 'Declare identity file':
   path  => '/etc/ssh/ssh_config',
-  line  => '    IdentityFile ~/.ssh/school',
-  match => '^.*IdentityFile.*$',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^IdentityFile',
 }
