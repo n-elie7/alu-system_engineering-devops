ae42308abd0cc6427130099a86e6392781a5f4b3
Elie <n.elie@alueducation.com>
2026-03-21 23:02:14 +0200

    Increases hard and soft file limits for the holberton user

--- web_stack_debugging_4/1-user_limit.pp
+++ web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,10 @@
-# Increases hard and soft file limits for the holberton user to fix "Too many open files" error
-exec { 'change-os-configuration-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf && sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+# Increases hard and soft file limits for the holberton user
+exec { 'increase-hard-file-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
+}
+
+exec { 'increase-soft-file-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
 }
